f5a9e9f9b7199803e61b8146bd83ece86346f295
Fernando Oliveira <47955596+lfpo2005@users.noreply.github.com>
2022-01-29 11:02:03 -0300

    Encapsulamento

---
+++ src/ContaCorrente.java
@@ -0,0 +1,29 @@
+
+public class ContaCorrente {
+
+    private int agencia;
+    private int conta;
+    private double saldo;
+
+    public void sacar(){
+
+    }
+    public void depositar(){
+
+    }
+    public void transferir(){
+
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getConta() {
+        return conta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+}
---
+++ src/ContaPopanca.java
@@ -0,0 +1,30 @@
+
+
+public class ContaPopanca {
+
+    private int agencia;
+    private int conta;
+    private double saldo;
+
+    public void sacar(){
+
+    }
+    public void depositar(){
+
+    }
+    public void transferir(){
+
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getConta() {
+        return conta;
+    }
+
+    public double getSaldo() {
+        return saldo;
+    }
+}
